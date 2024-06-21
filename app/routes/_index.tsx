4cbc44e9b1209a2fd7d3657b9736291462cba96a
Karl <karlcereno@gmail.com>
2024-06-20 20:46:02 -0400

    Move navLinks

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
 import { Link } from '@remix-run/react';
-import { navLinks } from '~/constants/links';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,6 +9,21 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const navLinks = [
+    {
+      name: 'Calculators',
+      links: [
+        {
+          name: 'One Rep Max',
+          url: '/calculators/one-rep-max',
+        },
+        {
+          name: 'Barbell Load ',
+          url: '/calculators/barbell-load',
+        },
+      ],
+    },
+  ];
   return (
     <main className="p-6 space-y-4 bg-base-200 min-h-screen">
       <div className="max-w-lg mx-auto">
